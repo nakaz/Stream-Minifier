d5c700203a75e31d4441c180c89bad4d5d7c4c19
Sean Nakamura <seanenakamura@gmail.com>
2015-08-20 00:23:28 -1000

    refactored to use a class minifyCSS

--- minify.js
+++ minify.js
@@ -1,35 +1,22 @@
-var Stream = require ('stream');
-var stream = new Stream;
-var fs = require('fs');
+var stream = require ('stream');
 
-var source = fs.createReadStream('./css/foundation.css', 'utf8');
-var product = fs.createWriteStream('./dump.css', 'utf8');
+var minified = fs.createWriteStream('./dump.css', 'utf8');
 
-var Readable = require('stream').Readable;
-var Transform = require('stream').Transform;
+var Transform = stream.Transform;
 
-source.setEncoding('utf8');
-
+var minifyCSS = new Transform();
 
 var string = '';
 
-source.pipe(product);
+minifyCSS._transform = function (chunk, encoding, done){
+  string += chunk;
+  string = string.replace(/\s/g, "");
+  string = string.replace(/(\/\*).*(\*\/)/g, "");
+  minified.write(string, 'utf8');
+}
 
-source.on('data', function (){
-  console.log('received data');
-});
+process.stdin
+  .pipe(minifyCSS)
+  .pipe(process.stdout);
 
-product.on('pipe', function (){
-  console.log('initiating pipe');
-});
-
-product.on('unpipe', function (){
-  console.log('finished pipe');
-});
-
-// process.stdin.on('data', function (chunk){
-//   string += chunk;
-//   string = string.replace(/\s/g, "");
-//   string = string.replace(/(\/\*).*(\*\/)/g, "");
-//   product.write(string);
-// });
+process.stdout.on('error', process.exit);
